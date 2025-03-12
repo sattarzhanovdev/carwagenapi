188f0b4db54b858a292fe747536cbe10ff385dc9
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-03-12 14:41:49 +0600

    push

--- cars/models.py
+++ cars/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class Car(models.Model):
-    brand = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-    year = models.IntegerField()
-    price = models.IntegerField()
-    photo = models.ImageField(upload_to='cars/')
+    Бренд = models.CharField(max_length=100)
+    Модель = models.CharField(max_length=100)
+    Год = models.IntegerField()
+    Цена = models.IntegerField()
+    Комплектация = models.IntegerField()
+    Фотография = models.ImageField(upload_to='cars/')
     class Meta:
         app_label = 'cars'
 
     def __str__(self):
-        return f"{self.brand} {self.model} {self.price} ({self.year})"
+        return f"{self.Бренд} {self.Модель} {self.Цена} {self.Комплектация} ({self.Год}) "
